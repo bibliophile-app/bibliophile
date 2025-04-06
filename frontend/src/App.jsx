b144e7ea240512371336449a2ebcc8300359916d
Júlia Calixto Rosa <juliacalixtorosaa@gmail.com>
2025-04-06 16:26:17 -0300

    feature: página cliente inicial com CRUD de usuários
    Signed-off-by: Júlia Calixto Rosa <juliacalixtorosaa@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import UserManager from "./components/UserManager";
+import QuoteManager from "./components/QuoteManager";
 import BooklistManager from './components/BooklistManager';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/test/booklists" element={<BooklistManager />} />
         <Route path="/test/users" element={<UserManager/>}/>
+        <Route path="/test/quotes" element={<QuoteManager/>}/>
       </Routes>
     </Router>
   );
