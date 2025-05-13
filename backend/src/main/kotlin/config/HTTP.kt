c506457bcd4c162e5238dfc3a241169e35785a68
Giovanna Uchôa <gio-uchoa@hotmail.com>
2025-05-13 16:47:26 -0300

    configurando allowed hosts para conseguir utilizar tuneis; configurando middleware para rotas
    Signed-off-by: Giovanna Uchôa <gio-uchoa@hotmail.com>
    
    consertando rota default para backend
    
    Signed-off-by: Giovanna Uchôa <gio-uchoa@hotmail.com>

--- backend/src/main/kotlin/config/HTTP.kt
+++ backend/src/main/kotlin/config/HTTP.kt
@@ -1,17 +1,14 @@
 package com.bibliophile.config
 
+import java.net.URI
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import org.slf4j.event.*
 
 fun Application.configureHTTP() {
+
+    val viteAllowedHost = System.getenv("VITE_ALLOWED_HOST")
+
     install(CORS) {
         allowCredentials = true
         allowHost("localhost:3000", schemes = listOf("http"))
@@ -22,5 +19,14 @@ fun Application.configureHTTP() {
         allowMethod(HttpMethod.Options)
         allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.Authorization)
+
+        viteAllowedHost?.let {
+            try {
+                val uri = URI("https://$it")
+                allowHost(uri.host, schemes = listOf(uri.scheme))
+            } catch (e: Exception) {
+                this@configureHTTP.log.warn("VITE_ALLOWED_HOST is not a valid URI: $it")
+            }
+        }
     }
 }
